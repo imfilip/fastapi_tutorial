b5f16fd50d524a2281c41c657fb25914c962c51e
Marcin Filip <filip.marcin17@gmail.com>
2022-10-08 14:00:53 +0200

    changing the world slightly

--- app/main.py
+++ app/main.py
@@ -36,4 +36,5 @@ app.include_router(votes.router)
 
 @app.get("/", tags = ["index"])
 async def root():
-    return {"message": "Hello World!"}
+    return {"message": "Hello World!",
+        "task": "Change your URL adding sufix /docs. Try the app out creating firstly a new user and next after the authentication (green button with padlock and 'Authorize' word) creating posts. Have fun dude! :)"}
